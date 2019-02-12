0b0a9c46fc09dcf2598ffa905091c70e10f46d7b
John Hay <john.paul.hay@outlook.com>
2019-02-12 14:59:54 +0000

    Set up email subscribe with mailJS

--- assets/js/sendEmail.js
+++ assets/js/sendEmail.js
@@ -1,15 +1,34 @@
-function sendMail(contactForm){
+function sendMail(contactForm) {
     emailjs.send("gmail", "action_inequality", {
-        "from_name": contactForm.name.value,
-        "from_email": contactForm.emailaddress.value,
-        "web_request": contactForm.projectsummary.value
-    })
-    .then(
-        function(response){
-            console.log("SUCCESS", response);
-        },
-        function(error){
-            console.log("FAILED", error)
-        
-        });
+            "from_name": contactForm.name.value,
+            "from_email": contactForm.emailaddress.value,
+            "web_request": contactForm.projectsummary.value
+        })
+        .then(
+            function(response) {
+                console.log("SUCCESS", response);
+            },
+            function(error) {
+                console.log("FAILED", error)
+
+            });
+
+}
+
+function submitEmailSubscription(emailSubscribe) {
+    emailjs.send("gmail", "ai_email_subscribe", {
+            "first_name": emailSubscribe.first_name.value,
+            "last_name": emailSubscribe.last_name.value,
+            "email_subscribed": emailSubscribe.email_subscribed.value
+
+
+        })
+        .then(
+            function(response) {
+                console.log("SUCCESS", response);
+            },
+            function(error) {
+                console.log("FAILED", error)
+
+            });
 }
