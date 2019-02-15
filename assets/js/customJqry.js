c76dca43d73b2a1f4adaa916b96996b862e65f7a
John Hay <john.paul.hay@outlook.com>
2019-02-15 15:16:24 +0000

    change content start with actual graph data

--- assets/js/customJqry.js
+++ assets/js/customJqry.js
@@ -4,8 +4,8 @@
 
 $(function() {
 
-var animationName = "animated shake";
-var animationEnd = "webkitAnimationEnd mozAnimationEnd MSAnimationEnd oanimationend animationend"
+    var animationName = "animated shake";
+    var animationEnd = "webkitAnimationEnd mozAnimationEnd MSAnimationEnd oanimationend animationend"
 
     $('h1').on('click', function() {
         $('h1').addClass(animationName).one(animationEnd,
@@ -15,52 +15,41 @@ var animationEnd = "webkitAnimationEnd mozAnimationEnd MSAnimationEnd oanimation
         $()
     });
 
-    $(document).ready(function(){
-  // $sections incleudes all of the container divs that relate to menu items.
-  var $sections = $('.nav-container');
-  
-  // The user scrolls
-  $(window).scroll(function(){
-    
-    // currentScroll is the number of pixels the window has been scrolled
-    var currentScroll = $(this).scrollTop();
-    
-    // $currentSection is somewhere to place the section we must be looking at
-    var $currentSection
-    
-    // We check the position of each of the divs compared to the windows scroll positon
-    $sections.each(function(){
-      // divPosition is the position down the page in px of the current section we are testing      
-      var divPosition = $(this).offset().top;
-      
-      // If the divPosition is less the the currentScroll position the div we are testing has moved above the window edge.
-      // the -1 is so that it includes the div 1px before the div leave the top of the window.
-      if( divPosition - 1 < currentScroll ){
-        // We have either read the section or are currently reading the section so we'll call it our current section
-        $currentSection = $(this);
-        
-        // If the next div has also been read or we are currently reading it we will overwrite this value again. This will leave us with the LAST div that passed.
-      }
-      
-      // This is the bit of code that uses the currentSection as its source of ID
-      var id = $currentSection.attr('id');
-   	 
-   	 
-   	 $('a').removeClass('active');
-   	 $(`a[href="#${id}"]`).addClass("active");
-      
-    })
 
-  });
-  
-  $("form").on("submit", function(event){
-      event.preventDefault();
-      console.log("hello");
-  })
-  
+
+
+
+    //Nav bar code
+
+
+    $(document).ready(function() {
+
+        var $sections = $('.nav-container');
+        $(window).scroll(function() {
+
+            var currentScroll = $(this).scrollTop();
+            var $currentSection
+            $sections.each(function() {
+
+                var divPosition = $(this).offset().top;
+                if (divPosition - 1 < currentScroll) {
+                    $currentSection = $(this);
+                }
+
+                var id = $currentSection.attr('id');
+                $('a').removeClass('active');
+                $(`a[href="#${id}"]`).addClass("active");
+            })
+        });
+
+        //Prevent default form reload behaviour
+
+        $("form").on("submit", function(event) {
+            event.preventDefault();
+
+        })
+
+
+    });
+
 });
-    
-});
-
-
-
