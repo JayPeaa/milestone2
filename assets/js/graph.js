e4885fa90eb7f91a018f82c21591fa55fc3606ed
John Hay <john.paul.hay@outlook.com>
2019-02-23 13:33:39 +0000

    Create line chart

--- assets/js/graph.js
+++ assets/js/graph.js
@@ -42,7 +42,7 @@ function makeGraphs(error, salesData) {
             .xAxisLabel("Country")
             .yAxisLabel("Sales in £'s")
             .yAxis().ticks(10);
-            
+
 
 
 
@@ -51,7 +51,7 @@ function makeGraphs(error, salesData) {
     function show_sales_by_manager(ndx) {
         var dim = ndx.dimension(dc.pluck("Manager"));
         var group = dim.group().reduceSum(dc.pluck('Sales'));
-        
+
         let barColors = d3.scale.ordinal().range(["#EA3500", "#EA3500", "#8CE888", "#8CE888", "#FEC928", "#FEC928", "#093F9B", "#282828"])
 
         dc.barChart("#chart2")
@@ -77,7 +77,7 @@ function makeGraphs(error, salesData) {
     function show_sales_by_category(ndx) {
         var dim = ndx.dimension(dc.pluck("Category"));
         var group = dim.group().reduceSum(dc.pluck('Sales'));
-        
+
         let barColors = d3.scale.ordinal().range(["#4C73B6"])
 
         dc.barChart("#chart5")
@@ -103,7 +103,7 @@ function makeGraphs(error, salesData) {
     function show_sales_by_chain(ndx) {
         var dim = ndx.dimension(dc.pluck("Chain"));
         var group = dim.group().reduceSum(dc.pluck('Sales'));
-        
+
         let barColors = d3.scale.ordinal().range(["#093F9B", "#8CE888"])
 
         dc.pieChart("#chart3")
@@ -116,30 +116,33 @@ function makeGraphs(error, salesData) {
             .dimension(dim)
             .group(group)
             .transitionDuration(1500)
-            
+
 
     }
 
     function show_sales_by_chain_line(ndx) {
-        var dim = ndx.dimension(dc.pluck("Chain"));
-        var group = dim.group().reduceSum(dc.pluck('Sales'));
-        
-        let barColors = d3.scale.ordinal().range(["#093F9B", "#8CE888"])
 
-        dc.barChart("#chart4")
-            .width(360)
+        var parseDate = d3.time.format("%d/%m/%Y").parse;
+        salesData.forEach(function(d) {
+            d.Month = parseDate(d.Month);
+        })
+
+        var month_dim = ndx.dimension(dc.pluck("Month"));
+        var total_sales_per_month = month_dim.group().reduceSum(dc.pluck("Sales"));
+
+        var minDate = month_dim.bottom(1)[0].Month;
+        var maxDate = month_dim.top(1)[0].Month;
+
+
+        dc.lineChart("#chart4")
+            .width(1000)
             .height(300)
-            .colorAccessor(function(d) {
-                return d.key
-            })
-            .colors(barColors)
-           .margins({ top: 10, right: 50, bottom: 40, left: 65 })
-            .dimension(dim)
-            .group(group)
+            .margins({ top: 10, right: 50, bottom: 40, left: 65 })
+            .dimension(month_dim)
+            .group(total_sales_per_month)
             .transitionDuration(500)
-            .x(d3.scale.ordinal())
-            .xUnits(dc.units.ordinal)
-            .xAxisLabel("Chain")
+            .x(d3.time.scale().domain([minDate, maxDate]))
+            .xAxisLabel("Month")
             .yAxisLabel("Sales in £'s")
             .yAxis().ticks(10);
 
@@ -154,7 +157,7 @@ function makeGraphs(error, salesData) {
     function show_sales_by_month(ndx) {
         var dim = ndx.dimension(dc.pluck("Financial Year"));
         var group = dim.group().reduceSum(dc.pluck('Sales'));
-        
+
         let barColors = d3.scale.ordinal().range(["#4C73B6"])
 
         dc.barChart("#chart0")
